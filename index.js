79d5e2ff03d481a51337c26cad4f30b9198f4ee0
KapilThaman <kapil.thaman.k@gmail.com>
2024-10-27 21:08:29 -0700

    Reverse Sting Question Solved

--- index.js
+++ index.js
@@ -7,4 +7,22 @@ function titleCase(Str){
     console.log(Strg.join(" "));
 }
 // capitalize all the first letter of the word.
-titleCase("hi my name is kapil")
+// titleCase("hi my name is kapil");
+
+function ReverseString(Strng){
+    const Word = Strng.split("");
+    let i = 0 ;
+    let j = Strng.length - 1;
+
+    while( i < j){
+        let temp = 0;
+        temp = Word[i];
+        Word[i] = Word[j];
+        Word[j] = temp;
+
+        i++;
+        j--;
+    }
+    console.log(Word.join(""));
+}
+ReverseString("Kapil");
